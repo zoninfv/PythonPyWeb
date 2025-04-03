defd3e23202e48bc22cb8c3a2c93880a6d80a20a
zonin.fv <zonin.fv@gmail.com>
2025-04-03 21:21:49 +0300

    03.04.2024 конец практики

--- apps/db_train/admin.py
+++ apps/db_train/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 # Зарегистрируйте свои модели в админ панели здесь
 from django.contrib import admin
-from .models import Author
+from .models import Author, Entry, Tag
 
 admin.site.register(Author)
+admin.site.register(Entry)
+admin.site.register(Tag)
